87c8cfdc845de8c3ad7513631d9a0ae131d5ccb5
a0b013g <anand.banik@walmart.com>
2020-05-25 22:53:37 -0500

    minor changes - removed logs

--- middleware/orchestrator.js
+++ middleware/orchestrator.js
@@ -4,10 +4,12 @@ module.exports = function (require) {
   let logger = require('log4js').getLogger('orchestrator');
 
   const ORG = process.env.ORG || null;
+  /*
   if (ORG !== 'insurance') {
     logger.info('enabled for gateway only');
     return;
   }
+  */
 
   let invoke = require('../lib-fabric/invoke-transaction.js');
   let peerListener = require('../lib-fabric/peer-listener.js');
@@ -16,49 +18,59 @@ module.exports = function (require) {
 
   peerListener.registerBlockEvent(block => {
     try {
-      block.data.data.forEach(blockData => {
 
-        logger.info(`got block no. ${block.header.number}`);
 
-        blockData.payload.data.actions.forEach(action => {
-          let extension = action.payload.action.proposal_response_payload.extension;
+      if (typeof block.data !== "undefined") {
 
-          let event = extension.events;
-          if(!event.event_name) {
-            return;
-          }
+        block.data.data.forEach(blockData => {
 
-          logger.trace(`event ${event.event_name}`);
 
-          if(event.event_name === 'registration') {
-            //let payload = JSON.parse(event.payload.toString());
-            let payload = event.payload.toString();
-            //logger.trace('registration', JSON.stringify(payload));
-            logger.trace('registration', payload)
-            moveByEvent(payload);
-          }
-        }); // thru action elements
-      }); // thru block data elements
+          logger.info('blockdata ->',blockData.payload);
+
+          blockData.payload.data.actions.forEach(action => {
+            let extension = action.payload.action.proposal_response_payload.extension;
+
+            let event = extension.events;
+            if (!event.event_name) {
+              logger.info('No events found');
+              return;
+            }
+
+            logger.trace(`event ${event.event_name}`);
+
+            if (event.event_name === 'registration') {
+              //let payload = JSON.parse(event.payload.toString());
+              let payload = event.payload.toString();
+              //logger.trace('registration', JSON.stringify(payload));
+              logger.trace('registration', payload)
+              moveByEvent(payload);
+            }
+          }); // thru action elements
+        }); // thru block data elements
+      } else {
+        logger.info("Block data is undefined");
+        return;
+      }
     }
-    catch(e) {
+    catch (e) {
       logger.error('caught while processing block event', e);
     }
   });
 
-  peerListener.eventHub.on('connected', function() {
+  peerListener.eventHub.on('connected', function () {
     logger.info('connected');
   });
 
   function moveByEvent(payload) {
-    
+
     let args = payload.split("@");
     logger.debug('invoking register of %s for %s', args[1], args[0]);
-    
+
     return invoke.invokeChaincode(['peer0.dmv.devpulsecon.com:7051'], 'register',
       'register', 'register', args, 'orchestrator', ORG)
       .then(transactionId => {
         logger.info('applied success', transactionId);
-        updateNodeService(payload)
+        //updateNodeService(payload)
       })
       .catch(e => {
         logger.error('application error', e);
@@ -72,16 +84,16 @@ module.exports = function (require) {
     var end_date = new Date();
     start_date.setMinutes(start_date.getMinutes() - start_date.getTimezoneOffset())
     end_date.setMinutes(start_date.getMinutes() - start_date.getTimezoneOffset())
-    end_date.setFullYear(start_date.getFullYear()+1)
-    var post_req  = null,
-    post_data = '{ "vin_number":"'+args[1]+'","status":"Apply","start_date":"'+start_date+'","end_date":"'+end_date+'","ssn_number":"'+args[0]+'"}';
+    end_date.setFullYear(start_date.getFullYear() + 1)
+    var post_req = null,
+      post_data = '{ "vin_number":"' + args[1] + '","status":"Apply","start_date":"' + start_date + '","end_date":"' + end_date + '","ssn_number":"' + args[0] + '"}';
 
     var post_options = {
-    hostname: 'codechix-service.devpulsecon.com',
-    port    : '8000',
-    path    : '/api/registration',
-    method  : 'POST',
-    headers : {
+      hostname: 'codechix-service.devpulsecon.com',
+      port: '8000',
+      path: '/api/registration',
+      method: 'POST',
+      headers: {
         'Content-Type': 'application/json',
         'Cache-Control': 'no-cache',
         'Content-Length': post_data.length
@@ -96,13 +108,13 @@ module.exports = function (require) {
       });
     });
 
-    post_req.on('error', function(e) {
+    post_req.on('error', function (e) {
       logger.info('problem with request: ' + e.message);
     });
 
     post_req.write(post_data);
     post_req.end();
 
-}
+  }
 
 };
